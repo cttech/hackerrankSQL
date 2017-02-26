f24bd5c186b66cb96627a4955452c86da8dee8b3
cttech <cttechbd@gmail.com>
2017-02-26 20:10:47 +0600

    basic sql problem solution

---
+++ Revising-the-Select-Query-I.sql
@@ -0,0 +1,2 @@
+ https://www.hackerrank.com/challenges/revising-the-select-query
+ SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' && POPULATION > 100000
---
+++ Revising-the-Select-Query-II.sql
@@ -0,0 +1,2 @@
+https://www.hackerrank.com/challenges/revising-the-select-query-2
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' && POPULATION > 120000
